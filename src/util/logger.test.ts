58ef742cc2fc0e9a858fb1405f47a062acee2797
Samir Alibabic <samir.alibabic@gmail.com>
2024-01-23 17:27:46 +0100

    test: Updated tests for logger Unable to test decorate function, so the tests are still commented out.

--- src/util/logger.test.ts
+++ src/util/logger.test.ts
@@ -1,6 +1,6 @@
-import { describe, expect, test } from "bun:test";
+import { describe, expect, test, jest, beforeEach, afterEach } from "bun:test";
 import * as path from "path";
-import { getRelativeFilePath } from "./logger.js";
+import { decorate, getRelativeFilePath } from "./logger.js";
 
 describe("getRelativeFilePath", () => {
   test("returns the relative file path", () => {
@@ -15,3 +15,92 @@ describe("getRelativeFilePath", () => {
     expect(relativeFilePath).toBe("src/util/files/fileUtils.ts");
   });
 });
+
+
+// describe("decorate", () => {
+//   let mockOriginalFunction;
+//
+//   beforeEach(() => {
+//     jest.spyOn(global, 'Error').mockImplementation(() => ({
+//       stack: undefined,
+//     }));
+//
+//     mockOriginalFunction = jest.fn();
+//   });
+//
+//   afterEach(() => {
+//     jest.spyOn(global, 'Error').mockRestore();
+//   });
+//
+//
+//   test("should call the original function with decorated arguments", () => {
+//     const mockArgs = ["arg1", "arg2"];
+//
+//     decorate(mockOriginalFunction, ...mockArgs as []);
+//
+//     expect(mockOriginalFunction).toHaveBeenCalledWith(
+//       expect.stringContaining("[mockOriginalFunction |"),
+//       ...mockArgs
+//     );
+//   });
+//
+//   test("should enhance the original function with timestamp, file path, and function name", () => {
+//     const mockArgs = ["arg1", "arg2"];
+//     const timestamp = new Date().toLocaleTimeString();
+//
+//     decorate(mockOriginalFunction, ...mockArgs as []);
+//
+//     expect(mockOriginalFunction).toHaveBeenCalledWith(
+//       expect.stringContaining(`[${mockOriginalFunction.name} | ${timestamp} | unknown]`), 
+//       ...mockArgs
+//     );
+//   });
+//
+//   test("should correctly decorate the 'log' function", () => {
+//     const mockArgs = ["arg1", "arg2"];
+//     const timestamp = new Date().toLocaleTimeString();
+//
+//     console.log(...mockArgs);
+//
+//     expect(mockOriginalFunction).toHaveBeenCalledWith(
+//       expect.stringContaining(`[log | ${timestamp} | unknown]`),
+//       ...mockArgs
+//     );
+//   });
+//
+//   test("should correctly decorate the 'error' function", () => {
+//     const mockArgs = ["arg1", "arg2"];
+//     const timestamp = new Date().toLocaleTimeString();
+//
+//     console.error(...mockArgs);
+//
+//     expect(mockOriginalFunction).toHaveBeenCalledWith(
+//       expect.stringContaining(`[error | ${timestamp} | unknown]`),
+//       ...mockArgs
+//     );
+//   });
+//
+//   test("should correctly decorate the 'info' function", () => {
+//     const mockArgs = ["arg1", "arg2"];
+//     const timestamp = new Date().toLocaleTimeString();
+//
+//     console.info(...mockArgs);
+//
+//     expect(mockOriginalFunction).toHaveBeenCalledWith(
+//       expect.stringContaining(`[info | ${timestamp} | unknown]`),
+//       ...mockArgs
+//     );
+//   });
+//
+//   test("should correctly decorate the 'warn' function", () => {
+//     const mockArgs = ["arg1", "arg2"];
+//     const timestamp = new Date().toLocaleTimeString();
+//
+//     console.warn(...mockArgs);
+//
+//     expect(mockOriginalFunction).toHaveBeenCalledWith(
+//       expect.stringContaining(`[warn | ${timestamp} | unknown]`),
+//       ...mockArgs
+//     );
+//   });
+// });
