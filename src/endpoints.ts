0905b757d59ea158de117e535a385590a0be2afc
Samir Alibabic <samir.alibabic@gmail.com>
2024-02-05 18:09:08 +0100

    fix: return 200 when OAuth is complete.

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -59,7 +59,7 @@ export async function notion(req: Request, hashionDb: Database): Promise<Respons
 
       insertIntoDb(hashionDb, userProfile);
 
-      return Response.redirect('/hashnode', 302);
+      return new Response('Ok. Notion data collected. Proceeed to /hashnode.', { status: 200 });
     } catch (error) {
       console.error('Could not exchange authorization code for access token.', (error as Error).message);
 
