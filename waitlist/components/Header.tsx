907a9804ea2d375626a069ed576f64a9f7e560d7
Samir Alibabic <samir.alibabic@gmail.com>
2024-02-08 10:50:05 +0100

    feat(waitlist): finish header layout

--- waitlist/components/Header.tsx
+++ waitlist/components/Header.tsx
@@ -1,24 +1,26 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header>
-      <div className='grid grid-cols-2 p-4 h-20 items-center border-b border-ghost'>
-        <div className='flex items-center'>
-          <div className='relative w-10 h-10 mr-2'>
+      <div className="grid grid-flow-col p-4 h-20 items-center border-b border-ghost">
+        <div className="flex items-center">
+          <div className="relative w-10 h-10 mr-2">
             <Image
-              src="logo-icon.svg"
+              src="/logo-icon.svg" 
               alt="Hasion logo"
               fill
             />
           </div>
-          <span className='grid items-center h-10 text-xl text-black-pearl font-bold tracking-wide'>hashion</span>
+          <span className="grid items-center h-10 text-xl text-black-pearl font-bold tracking-wide">
+            hashion
+          </span>
         </div>
-        <menu className='grid grid-cols-2 text-right text-pale-sky'>
-          <li>Features</li>
-          <li>Contact</li>
+        <menu className="flex justify-end space-x-8 text-pale-sky">
+          <li className="text-right">Features</li>{" "}
+          <li className="text-right">Contact</li>{" "}
         </menu>
       </div>
     </header>
-  )
+  );
 }
